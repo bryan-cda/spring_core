cd3a541db5a9b72d8e9299b9b3421967ee2b4444
Bryan Albuquerque <balbuquerque@sccalpha.local>
2021-10-12 20:24:46 -0300

    Implements all other common calculator operartions.

--- spring-aws-ms-intellij/spring-aws-ms/src/main/java/br/com/springawsms/controller/CalculatorController.java
+++ spring-aws-ms-intellij/spring-aws-ms/src/main/java/br/com/springawsms/controller/CalculatorController.java
@@ -19,14 +19,46 @@ public class CalculatorController {
         return new Greeting(id.getAndIncrement(), String.format(greeting, name));
     }
 
-    @GetMapping(value = "/sum/{numberOne}/{numberTwo}")
+    @GetMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(@PathVariable ("numberOne") String numberOne,  @PathVariable ("numberTwo") String numberTwo){
        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
            throw new NumberFormatException(String.format("Incorrect number passed: %s, %s", numberOne, numberTwo));
        }
-        double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
-        return sum;
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
+
+    @GetMapping("/subtract/{numberOne}/{numberTwo}")
+    public Double subtract(@PathVariable ("numberOne") String numberOne,  @PathVariable ("numberTwo") String numberTwo){
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new NumberFormatException(String.format("Incorrect number passed: %s, %s", numberOne, numberTwo));
+        }
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    @GetMapping("/division/{numberOne}/{numberTwo}")
+    public Double division(@PathVariable ("numberOne") String numberOne,  @PathVariable ("numberTwo") String numberTwo){
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new NumberFormatException(String.format("Incorrect number passed: %s, %s", numberOne, numberTwo));
+        }
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+    @GetMapping("/multiplication/{numberOne}/{numberTwo}")
+    public Double multiplication(@PathVariable ("numberOne") String numberOne,  @PathVariable ("numberTwo") String numberTwo){
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new NumberFormatException(String.format("Incorrect number passed: %s, %s", numberOne, numberTwo));
+        }
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    @GetMapping("/square_root/{number}")
+    public Double squareRoot(@PathVariable ("number") String number){
+        if(!isNumeric(number)){
+            throw new NumberFormatException(String.format("Incorrect number passed: %s", number));
+        }
+        return (Double) Math.sqrt(Double.parseDouble(number));
+    }
+
 
     public Double convertToDouble(String number){
         if(Objects.isNull(number)) return 0D;
