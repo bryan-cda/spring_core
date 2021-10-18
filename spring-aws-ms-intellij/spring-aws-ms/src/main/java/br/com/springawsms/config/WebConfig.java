ec6adfe6ca94ad7a635f22a63e04293ad00eec7e
Bryan Albuquerque <balbuquerque@sccalpha.local>
2021-10-18 16:44:30 -0300

    Implemented content negotiation with media type xml and json.

---
+++ spring-aws-ms-intellij/spring-aws-ms/src/main/java/br/com/springawsms/config/WebConfig.java
@@ -0,0 +1,16 @@
+package br.com.springawsms.config;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+    }
+}
