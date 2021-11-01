2b6bb0df65eb62680ab92aaaeafeb9a2f17a5e67
Bryan Duarte <albuquerque.bry.n@gmail.com>
2021-11-01 14:58:27 -0300

    Update AccountVO class.

--- spring-aws-ms-intellij/spring-aws-ms/src/main/java/br/com/springawsms/vo/AccountCredentialsVO.java
+++ spring-aws-ms-intellij/spring-aws-ms/src/main/java/br/com/springawsms/vo/AccountCredentialsVO.java
@@ -1,10 +1,12 @@
 package br.com.springawsms.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class AccountCredentialsVO  {
     private String userName;
     private String password;
