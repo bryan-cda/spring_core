a9f3363065f15bb03c4b0609e0357e02e09c81ed
Bryan Duarte <bryan.duarte@ext.valid.com>
2022-05-01 21:59:51 -0300

    :hammer: [build] Create a custom exception to file not found and problem to storage files.

---
+++ spring-aws-ms/src/main/java/br/com/spring/meetapp/exeption/FileStorageException.java
@@ -0,0 +1,17 @@
+package br.com.spring.meetapp.exeption;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class FileStorageException extends RuntimeException {
+
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message);
+    }
+}
