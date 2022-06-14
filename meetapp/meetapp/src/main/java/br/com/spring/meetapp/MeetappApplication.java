ebc01284ef14e5eccd71de854d473b82e9aa5275
Bryan Duarte <bryan.duarte@ext.valid.com>
2022-06-14 10:41:23 -0300

    :seedling: [build]: initial commit.

---
+++ meetapp/meetapp/src/main/java/br/com/spring/meetapp/MeetappApplication.java
@@ -0,0 +1,13 @@
+package br.com.spring.meetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetappApplication.class, args);
+	}
+
+}
