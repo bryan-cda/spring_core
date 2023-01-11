282f300106700f006dcef45f6e9a377a2ef5869d
Bryan Duarte <56884795+bryan-cda@users.noreply.github.com>
2023-01-11 10:27:36 -0300

    Delete spring-aws-ms directory

--- spring-aws-ms/src/main/resources/db/migration/V1__CREATE_PERSON_TABLE.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `person` (`id` bigint NOT NULL AUTO_INCREMENT,`address` varchar(255) DEFAULT NULL,
-    `first_name` varchar(20) NOT NULL,
-    `gender` varchar(30) DEFAULT NULL,
-    `last_name` varchar(80) NOT NULL,
-    PRIMARY KEY (`id`)
-    )
--- spring-aws-ms/src/main/resources/db/migration/V6__DUMP_PERMISSION_TABLE.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `permission` (`description`) VALUES
-                                             ('ADMIN'),
-                                             ('MANAGER'),
-                                             ('COMMON_USER');
