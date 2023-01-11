66fd1e9318f5b500a46721ffdfe914f47a5efb16
Bryan Duarte <albuquerque.bry.n@gmail.com>
2023-01-11 10:24:09 -0300

    :hammer: initial commit - setup using gradle.

---
+++ webflux/webflux/src/test/java/br/com/spring/webflux/WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
