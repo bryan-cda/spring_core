3834e203ee93acb936e9c8159d8953d6b045c6a7
Bryan Duarte <albuquerque.bry.n@gmail.com>
2023-01-12 18:52:47 -0300

    :hammer: implements a find data schedule method to list all quotes.

--- webflux/webflux/src/main/java/br/com/spring/webflux/domain/Quote.java
+++ webflux/webflux/src/main/java/br/com/spring/webflux/domain/Quote.java
@@ -5,10 +5,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "QUOTE")
 public class Quote {
--- webflux/webflux/src/main/java/br/com/spring/webflux/repository/QuoteRepository.java
+++ webflux/webflux/src/main/java/br/com/spring/webflux/repository/QuoteRepository.java
@@ -2,14 +2,19 @@ package br.com.spring.webflux.repository;
 
 import br.com.spring.webflux.domain.Quote;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface QuoteRepository extends PagingAndSortingRepository<Quote, Long> {
+public interface QuoteRepository extends PagingAndSortingRepository<Quote, Long>, CrudRepository<Quote, Long> {
     @RestResource(rel = "quotes", path = "quotes")
     List<Quote> findAllBySymbol(@Param("quote") String quote, Pageable pageable);
+
+    Optional<Quote> findFirstBySymbolOrderByTimestampDesc(String symbol);
 }
